34d6faaf1933a0cc1e2ead97530a4e9bd53b2aa2
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-02-24 12:34:47 +0530

    [Ayushi] Add .Added UC-7 Addition with target Unit Specification

--- src/main/java/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp.java
@@ -1,32 +1,13 @@
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        Length foot = new Length(1.0, Length.LengthUnit.FEET);
-        Length inches = new Length(12.0, Length.LengthUnit.INCH);
-        Length yard = new Length(1.0, Length.LengthUnit.YARDS);
-        Length cm = new Length(30.48, Length.LengthUnit.CENTIMETER);
-
-        System.out.println("1 feet = 12 inches " + foot.equals(inches) +
-                            "\n1 yard = 3 feets " + yard.equals(new Length(3.0, Length.LengthUnit.FEET)) +
-                            "\n30.48cm = 1 feet " + cm.equals(foot));
-
         Length feet = new Length(1.0, Length.LengthUnit.FEET);
-        Length inch = new Length(2.0, Length.LengthUnit.INCH);
+        Length inch = new Length(12.0, Length.LengthUnit.INCH);
 
-        Length result1 = feet.add(inch);
-        System.out.println("1 feet + 2 inches = " + result1);
-     
-        Length result2 = inch.add(feet);
-        System.out.println("2 inches + 1 feet = " + result2);
+        System.out.println("Result (default Length.LengthUnit): " + feet.add(inch));
 
-        Length yd = new Length(1.0, Length.LengthUnit.YARDS);
-        Length feetTwo = new Length(2.0, Length.LengthUnit.FEET);
-
-        Length result3 = yd.add(feetTwo);
-        System.out.println("1 yard + 2 feets = " + result3);
-        Length zero = new Length(0.0, Length.LengthUnit.FEET);
-        Length negative = new Length(-2.0, Length.LengthUnit.FEET);
-
-        System.out.println("1 feet + 0 feet = " + feet.add(zero));
-        System.out.println("5 feet + (-2 feet) = " + new Length(5.0, Length.LengthUnit.FEET).add(negative));
+        System.out.println("Target FEET: " + Length.add(feet, inch, Length.LengthUnit.FEET));
+        System.out.println("Target INCH: " + Length.add(feet, inch, Length.LengthUnit.INCH));
+        System.out.println("Target YARDS: " + Length.add(feet, inch, Length.LengthUnit.YARDS));
+        System.out.println("Target CM: " + Length.add(feet, inch, Length.LengthUnit.CENTIMETER));
     }
 }
