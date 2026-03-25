77d8343f3e3177fd44ad34fdcf8a212812e31184
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-03-25 15:15:42 +0530

    [Ayushi] Add .Added Spring Backend Security

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementApp.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
+        
         SpringApplication.run(QuantityMeasurementApp.class, args);
+    
     }
 }
