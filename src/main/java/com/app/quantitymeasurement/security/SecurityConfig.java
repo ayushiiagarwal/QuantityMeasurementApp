ce1cee937ae120478b3242e34dec5ab4b4d155e0
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-04-13 22:59:58 +0530

    [Ayushi] Refactor .Integrated Swagger

--- src/main/java/com/app/quantitymeasurement/security/SecurityConfig.java
+++ src/main/java/com/app/quantitymeasurement/security/SecurityConfig.java
@@ -61,8 +61,7 @@ public class SecurityConfig {
                     "/v3/api-docs/**",
                     "/v3/api-docs",
                     "/swagger-resources/**",
-                    "/webjars/**",
-                    "/**"  ).permitAll()
+                    "/webjars/**").permitAll()
                 .anyRequest().authenticated()
             )
             .exceptionHandling(ex -> ex
