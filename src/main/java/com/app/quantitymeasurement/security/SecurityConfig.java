e0d0040ee8173b56e54e0d207c79c6b96f7b97b5
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-04-13 23:46:55 +0530

    [Ayushi] Refactor .Integrated Swagger

--- src/main/java/com/app/quantitymeasurement/security/SecurityConfig.java
+++ src/main/java/com/app/quantitymeasurement/security/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> 
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
